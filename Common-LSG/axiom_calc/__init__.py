91d298d023ae3cc541f5c3b835e9f77ef313e3c5
郭洋洋-0009-0006-6669-1790 <811307785@qq.com>
2026-07-05 21:15:25 +0800

    Create __init__.py

---
+++ Common-LSG/axiom_calc/__init__.py
@@ -0,0 +1,13 @@
+from .lsg_axiom_core import (
+    section_mapping,
+    layer_gradient_integral,
+    dimension_reciprocal_transform,
+    dual_force_gradient
+)
+
+__all__ = [
+    "section_mapping",
+    "layer_gradient_integral",
+    "dimension_reciprocal_transform",
+    "dual_force_gradient"
+]
